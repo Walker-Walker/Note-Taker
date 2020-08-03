92e8a22f6611547af51cad32788758d8ff82de58
Walker-Walker <awalker0@gmail.com>
2020-08-03 16:31:33 -0700

    added lib file db directory to call getNotes function to be utilized by get requst in apiRoutes.js

---
+++ db/lib.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const util = require('util');
+
+
+class Lib {
+    getNotes(){
+        //get all the notes data from db.json and return it
+    }
+}
+
+
+
+
+
+
+module.exports = new Lib();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,12 @@
 const router = require("express").Router();
+const Lib = require('../db/lib');
+
+router.get('/notes', (req, res) => {
+    Lib.getNotes()//.then() return a promise ...send those notes back to the user..res.json send response to user 
+
+ });
+ 
+
 
 
 module.exports = router;
