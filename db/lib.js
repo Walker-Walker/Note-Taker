095165809d2c209dc3794b49c919907fcb8984fc
Walker-Walker <awalker0@gmail.com>
2020-08-09 13:07:34 -0700

    added ID generator module uuid, also reformatted promises in helper functions

--- db/lib.js
+++ db/lib.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const util = require("util");
 const rFAsync = util.promisify(fs.readFile);
 const wFAsync = util.promisify(fs.writeFile);
+const { v4: uuidv4} = require('uuid');
 
 class Lib {
   //   readNotesDataBase() {
@@ -23,33 +24,38 @@ class Lib {
 
   getNotesByQuery() {
     //parse returning data from readNotes function
-    return this.readNotesDataBase().then(function (note) {
-      const notes = JSON.parse(note);
+    return this.readNotesDataBase();
+    
+    /*.then(function (notes) {
+      const notes = JSON.parse(notes);
       return notes;
-    });
+    });*/
   }
 
   writeNotes(note) {
     // add it to the collection
-    wFAsync("db/db.json", note, function (err) {
-      if (err) {
-        return console.log(err);
-      }
-    });
-    // return wFAsync("db/db.json", "utf8")
+    return wFAsync("db/db.json", JSON.stringify(note));
+   
   }
 
   writeNotesToDataBase(note) {
     // read file then write to it
+    // generate a unique Id for the element to add to your DB
+    note.id = uuidv4(note);
+   // write the new collection to the database using a helper in your lib
+   
     return this.getNotesByQuery()
-      .then(function (notes) {
+      .then((notes) => {
         notes.push(note);
-        writeNotes(note);
+        return this.writeNotes(notes);
       })
-      .then(function (note) {
+      .then(() => {
         return note;
+      }).catch ((error) => {
+          console.error(error);
       });
-  }
+  };
+ 
 }
 
 module.exports = new Lib();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,20 +6,22 @@ router.get('/notes', (req, res) => {
     // don't make any unnecessary trips to the database
     // es6 lame version return req.query ? res.json( getNotesByQuery(req.query, notes) ) : res.json(notes);
     let results = notes;
-    if (req.query) {
+    /*if (req.query) {
         results = lib.getNotesByQuery(req.query, results);
-    }
+    }*/
    res.json(results);  
  });
     
 router.post('/notes', (req, res) => {
     // console.log(req)// what is this?
-     // generate a unique Id for the element to add to your DB
-     const notes = req.body;
-     notes.id = (notes.length);
-    // write the new collection to the database using a helper in your lib
-    const result = lib.writeNotesToDataBase(notes)
-    res.json(result)
+    const notes = req.body;
+
+    lib.writeNotesToDataBase(notes)
+    .then((notes) => {
+        res.json(result)
+    })
+    
+ 
 })
 
 //     Lib.getNotes()//.then() return a promise ...send those notes back to the user..res.json send response to user 
